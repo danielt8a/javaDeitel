3c93861e0ae9772ab039f3a52a4c716278a4c5b2
DamasoDaniel <damasodt8a@gmail.com>
2019-06-05 13:59:37 -0500

    Agregado ejercicio Muestra el valor entero de un caracter

---
+++ ValorEnteroDeCaracter.java
@@ -0,0 +1,15 @@
+// Muestra el valor entero de un caracter;
+
+public class ValorEnteroDeCaracter{
+	
+	public static void main(String[] args){
+
+		//Operador de conversión de tipo: (int) 'A'
+
+		System.out.printf("El caracter %c tiene el valor %d\n", 'A', ((int) 'A'));
+		System.out.printf("El caracter %c tiene el valor %d\n", 'B', ((int) 'B'));
+
+
+	}
+
+}
