e2be64dd16a71eb7173752fa05f09f86bef65720
DamasoDaniel <damasodt8a@gmail.com>
2019-06-05 13:22:37 -0500

    Conversión de int to float - Implicit casting

--- AreaCirculo.java
+++ AreaCirculo.java
@@ -1,2 +1,36 @@
 /*Aplicación que reciba del usuario el radio de un círculo como un entero,
 y que imprima el diámetro, circunferencia y el área del círculo. */
+import java.util.Scanner; // Recibe datos de entrada, es necesaria la clase Scanner
+
+public class AreaCirculo {
+
+	//Método main
+	public static void main(String[] args){
+
+		//Creación del objeto Scanner
+
+		Scanner entrada = new Scanner(System.in);
+
+		//Declaracion de variables
+		int radio = 5;
+
+		//Solicitud del valor del radio
+		System.out.println("Ingresa el valor del radio");
+		radio = entrada.nextInt();
+
+		//Implicit casting
+		float fradio = radio;
+
+
+		// Mostrar en pantalla el valor del diámetro, circunferencia y área
+
+		System.out.printf("El valor del diámetro es: %.3f\n", 2 * fradio);
+		System.out.printf("El valor de la circunferencia es: %.3f\n", 2 * Math.PI * fradio);
+		System.out.printf("El valor del área es: %.3f\n", Math.PI * (fradio * fradio)) ;
+
+
+
+
+
+	}
+}
