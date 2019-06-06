33d465a17dab6529f0945c39246fdbb11b1ee4f3
DamasoDaniel <damasodt8a@gmail.com>
2019-06-06 10:35:57 -0500

    Programa que descompone un número en sus dígitos individuales

---
+++ Separacion.java
@@ -0,0 +1,22 @@
+import java.util.Scanner
+
+public class SeparacionDigitos {
+	
+	public static void main(String[] args){
+
+		//Creación del objeto Scanner
+
+		Scanner entrada = new Scanner(System.in);
+
+		// Declaración de la variable número
+
+		int numero;
+
+		// Solicitud del número al usuario
+
+		System.out.println("Introduce un número de 5 dígitos: ");
+
+
+
+	}
+}
