8f2a8306bacef2067fcb674ca83a2b876ccac8e7
DamasoDaniel <damasodt8a@gmail.com>
2019-06-06 12:20:00 -0500

    Programa que descompone un número en sus dígitos individuales

--- Separacion.java
+++ Separacion.java
@@ -1,6 +1,6 @@
-import java.util.Scanner
+import java.util.Scanner;
 
-public class SeparacionDigitos {
+public class Separacion {
 	
 	public static void main(String[] args){
 
@@ -16,7 +16,29 @@ public class SeparacionDigitos {
 
 		System.out.println("Introduce un número de 5 dígitos: ");
 
+		numero = entrada.nextInt();
+
+		
+		// Si el número es mayor o menor a 5 dígitos
+		if (numero < 10000)
+			System.out.println("Por favor, introduce un número de 5 dígitos");
+
+		if (numero > 99999)
+			System.out.println("Por favor, introduce un número de 5 dígitos");
+
+		// Comprobar que el número es de 5 dígitos
+		
+		if (numero >= 10000 & numero <= 99999){
+					System.out.printf("%d %d %d %d %d \n", numero / 10000, 
+						(numero % 10000) / 1000,
+						(numero % 1000) / 100,
+						(numero % 100) / 10,
+						(numero % 10) / 1
+						);
+		}
 
 
 	}
+
 }
+		
