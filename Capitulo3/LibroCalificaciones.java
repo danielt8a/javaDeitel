c4a4e29bdffe5cdcb329a1fdeea05ef343575539
DamasoDaniel <damasodt8a@gmail.com>
2019-06-06 22:33:08 -0500

    Se creó la clase LibroCalificaciones

---
+++ Capitulo3/LibroCalificaciones.java
@@ -0,0 +1,12 @@
+// Fig 3.1: LibroCalificaciones.java
+// Declaración de un clase con un método
+
+public class LibroCalificaciones{
+
+	// Muestra el mensaje de bienvenida  al usuario de LibroCalificaciones
+
+	public void mostrarMensaje()
+	{
+		System.out.println("Bienvenido al Libro de Calificaciones!");
+	} // Fin del método mostrarMensaje
+}// Fin de la clase LibroCalificaciones
