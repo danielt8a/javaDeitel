03fe8484de60741c7718ad58d9d921694d5883ba
DamasoDaniel <damasodt8a@gmail.com>
2019-06-07 11:23:16 -0500

    Clase PruebaLibroCalificaciones que contiene el método main

---
+++ Capitulo3/PruebaLibroCalificaciones.java
@@ -0,0 +1,14 @@
+//Crea un objeto LibroCalificaciones y llama a su método mostrarMensaje
+
+public class PruebaLibroCalificaciones{
+
+	public static void main(String[] args){
+
+		//Crea un objeto LibroCalificaciones y lo asigna a miLibroCalificaciones
+		LibroCalificaciones miLibroCalificaciones = new LibroCalificaciones();
+
+		// Llama al método mostrar mensaje de miLibroCalificaciones
+		miLibroCalificaciones.mostrarMensaje();
+	} // Fin de main
+
+} //Fin de la clase PruebaLibroCalificaciones
