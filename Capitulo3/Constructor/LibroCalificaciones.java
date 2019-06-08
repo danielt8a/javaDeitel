0065d01bb421fc624848aee5c5116e4fc8c611f6
DamasoDaniel <damasodt8a@gmail.com>
2019-06-08 14:18:44 -0500

    Se agregó la variable para guardar el nombre del profesor así como sus métodos

--- Capitulo3/Constructor/LibroCalificaciones.java
+++ Capitulo3/Constructor/LibroCalificaciones.java
@@ -4,13 +4,16 @@
 public class LibroCalificaciones {
 
 	private String nombreDelCurso; // Nombre del libro para este LibroCalificaciones
+	private String nombreDelInstructor; // Nombre del instructor
 
 	// El constructor inicializa nombreDelCurso con un argumento String
 	//El nombre del constructor es el nombre de la Clase
 
-	public LibroCalificaciones(String nombre){ 
+	public LibroCalificaciones(String nombre, String instructor){ 
 
 		nombreDelCurso = nombre; // Inicializa nombreDelCurso
+		nombreDelInstructor = instructor; // Inicializa el nombreDelInstructor
+
 	} // Fin del constructor
 
 	// Método para establecer el nombre del Curso
@@ -27,6 +30,19 @@ public class LibroCalificaciones {
 		return nombreDelCurso;
 	} // Fin del método obtenerNombreDelCurso
 
+	// Método para establecer el nombre del Instructor
+
+	public void establecerNombreDelInstructor(){
+
+		nombreDelInstructor = instructor;
+	} // Fin del método establecerNombreDelInstructor
+
+	// Método obtener nombre del instructor
+
+	public String obtenerNombreDelInstructor(){
+		return nombreDelInstructor;
+	} //Fin del método obtenerNombreDelInstructor
+
 	//Muestra un mensaje de bienvenida al usuario
 
 	public void mostrarMensaje(){
@@ -34,6 +50,8 @@ public class LibroCalificaciones {
 		// Invoca a obtenerNombreDelCurso para otener el nombre del curso
 		System.out.printf("Bienvenido al libro de Calificaciones para: \n %s! \n",
 			obtenerNombreDelCurso() );
+		System.out.printf("Tu profesor asignado es: \n %s! \n",
+			obtenerNombreDelInstructor() );
 
 	} // Fin de mostrar mensaje
 
