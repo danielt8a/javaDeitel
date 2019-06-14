311742d9eb4e13b3d557a194f8688a5e96719d33
DamasoDaniel <damasodt8a@gmail.com>
2019-06-13 21:37:49 -0500

    Uso de JOptionPane para imprimir varias líneas en un cuadro de diálogo

---
+++ Capitulo3/swing/Dialogo1.java
@@ -0,0 +1,15 @@
+//Uso de JOptionPane para imprimir varias líneas en un cuadro de diálogo
+
+import javax.swing.JOptionPane; // Importa la clase JOptionPane
+
+public class Dialogo1 {
+
+	//El método main comienza la ejecución del programa
+	public static void main(String[] args){
+
+		//Muestra un cuadro de diálogo con un mensaje
+		
+
+	}
+
+}
